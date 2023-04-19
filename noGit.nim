f3bd590b3d644b80c375f4ec5736ee7953c84050
robbryandev <114073557+robbryandev@users.noreply.github.com>
2023-04-19 14:43:31 -0700

    Delete noGit.nim

--- noGit.nim
+++
@@ -1,9 +0,0 @@
-import std/[os, strutils]
-
-for i in os.walkDirs("./*"):
-    for f in os.walkDirs(i & "/*"):
-        echo "found " & f
-        if f.endsWith(".git"):
-            echo "removing " & f
-            os.removeDir(f)
-
